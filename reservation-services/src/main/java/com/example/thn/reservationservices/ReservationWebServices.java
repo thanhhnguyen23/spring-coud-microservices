086fc456141fcf160338979270699025a0557c15
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-14 23:26:54 -0400

    add reservation data service

---
+++ reservation-services/src/main/java/com/example/thn/reservationservices/ReservationWebServices.java
@@ -0,0 +1,28 @@
+package com.example.thn.reservationservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations") 
+public class ReservationWebServices {
+  private final ReservationRepository repository;
+
+  public ReservationWebServices(ReservationRepository repository) {
+    super();
+    this.repository = repository;
+  }
+
+  @GetMapping
+  public Iterable<Reservation> getAllReservations(){
+    return this.repository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Reservation getReservation(@PathVariable("id") long id){
+    return this.repository.findById(id).get();
+  }
+  
+}
